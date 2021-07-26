a20115587e1849a14154a91d2c342fdfe376d759
Viet <vietnamtran@gmail.com>
2021-07-25 20:34:50 -0500

    Increase font size

--- src/Details/Details.js
+++ src/Details/Details.js
@@ -10,16 +10,17 @@ const styles = makeStyles(theme => ({
   },
   title: {
     fontFamily: 'Skranji',
-    fontSize: '48px',
+    fontSize: '3.9vw',
     color: 'white',
     textAlign: 'center',
     width: '75%',
-    marginTop: '',
+    // marginTop: '',
     marginLeft: 'auto',
     marginRight: 'auto',
   },
   content: {
     fontFamily: 'Open Sans',
+    fontSize: '1.4vw',
     width: '50%',
     color: 'white',
     marginLeft: 'auto',
@@ -31,15 +32,6 @@ const Details = () => {
   const classes = styles();
   return (
     <Grid className={classes.container} container spacing={2} id='details'>
-      {/* <Box className="about-text">
-                <h1 className="section-header">About me</h1>
-                <Typography className="section-tagline">I am the person on your team who will lead when necessary and am comfortable in the back seat so others can drive. As a generalist I know a little bit about a lot of things. This allows me to move in the realms of the frontend, backend, as well as devops. I continue to grow instead of staying in a fixed state, so as tech changes so will I. My preference is to work for companies that make the lives of everyone better while also helping to solve some of our problems. I promise to do what I can to aid in the fight against the Galactic Empire.</Typography><br/>
-                <Typography className="section-tagline">When I’m not making click-ity clack-ity sounds with my keyboard and mouse I’ll be climbing, hopefully on rocks or otherwise on plastic. I love the desert and, fun fact, being envenomed does not make you a Parselmouth. Let’s nerd out or talk sport ball.</Typography>
-            </Box>
-            
-            <Box className="about-img">
-                <img src={headshot} alt="Viet Tran headshot" id="me" />
-            </Box> */}
       <Grid item md={6}>
         <Box className={classes.title}>About Me</Box>
         <p className={classes.content}>I grew up in Michigan, moved to San Francisco after graduating from college, and taught English in South Korea for 5 years. A generalist who enjoys learning a little bit about a lot of things. Continually growing and improving.</p>
